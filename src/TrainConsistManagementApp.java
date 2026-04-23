f62d228e7476f1c5882462bcb95b653c7f46b64e
NIkhil <nikhilkumarbeera2k6@gmail.com>
2026-04-23 13:25:50 +0530

    UC17: Sort Bogie Names Using Arrays.sort()

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,46 +1,36 @@
 /**
  * MAIN CLASS - TrainConsistManagementApp
- * UC16: Sort Passenger Bogies by Capacity (Bubble Sort)
+ * UC17: Sort Bogie Names Using Arrays.sort()
  * @author Nikhil
- * @version 16.0
+ * @version 17.0
  */
+
+import java.util.Arrays;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
-        int capacities[] = {72, 56, 24, 70, 60};
+        String bogies[] = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
         System.out.println("=======================================");
-        System.out.println("UC16 Manual Sorting using Bubble Sort\n");
+
+        System.out.println("UC17 Sort Bogie Names Using Arrays.sort()\n");
         System.out.println("=======================================");
 
-        System.out.println("Original Capacities:\n");
+        System.out.println("Original Bogie Names:\n");
+        System.out.println(Arrays.toString(bogies));
 
-        for(int i=0; i<capacities.length; i++) {
-            System.out.print(capacities[i] + " ");
-        }
+        Arrays.sort(bogies);
 
-        // Bubble Sort Logic
-        for(int i=0; i<capacities.length-1; i++) {
-            for(int j=0; j<capacities.length-1-i; j++) {
+        System.out.println("\nSorted Bogie Names (Alphabetical):\n");
+        System.out.println(Arrays.toString(bogies));
 
-                if(capacities[j] > capacities[j+1]) {
-
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j+1];
-                    capacities[j+1] = temp;
-                }
-            }
-        }
-
-        System.out.println("\n");
-        System.out.print("Sorted Capacities (Ascending): ");
-
-        for(int i=0; i<capacities.length; i++) {
-            System.out.print(capacities[i] + " ");
-        }
-
-        System.out.println("\n");
-        System.out.println("UC16 sorting completed.");
+        System.out.println("\nUC17 sorting completed...");
     }
 }
